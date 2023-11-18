79039324ebb834ca3244dc7a2e6be0bf7926e5fd
BodiReddyGayathri <151203172+BodiReddyGayathri@users.noreply.github.com>
2023-11-18 17:04:53 +0000

    login new added

--- app.py
+++ app.py
@@ -2,28 +2,6 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-JOBS=[
-  {
-  'id': 1,
-  'title': 'Python Developer',
-  'location':'banglore',
-  'salary':'rs 150000'
-},
-  {
-    'id': 2,
-    'title': 'frontend designer',
-    'location':'newyork',
-    'salary':'$50000'
-  },
-
-{
-  'id': 3,
-  'title': 'backend Developer',
-  'location':'texas',
-  'salary':'250000$'
-}
-
-]
 
 @app.route("/")
 def hello_world():
